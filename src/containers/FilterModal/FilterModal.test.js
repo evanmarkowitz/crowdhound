b93d502b9bed6af98b7e2266c0167603db0b3cf5
Evan Markowitz <emarkowitz1@gmail.com>
2019-09-02 17:03:16 -0600

    Wrote Snapshot for Filter Modal

---
+++ src/containers/FilterModal/FilterModal.test.js
@@ -0,0 +1,14 @@
+import { shallow } from 'enzyme';
+import FilterModal from './FilterModal'
+import React from 'react'
+
+describe('FilterModal', () => {
+  let wrapper
+  beforeEach(() => {
+    wrapper = shallow(<FilterModal />)
+  })
+  
+  it('should match the snapshot', () =>
+    expect(wrapper).toMatchSnapshot()
+  )
+})
