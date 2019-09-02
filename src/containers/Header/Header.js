6f70cb35dc69897f22afaabc035095d7d5a75f64
“Hindreen” <hndfaw@gmail.com>
2019-09-02 16:41:15 -0600

    Add space for carsouel to home page

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -3,10 +3,15 @@ import './Header.css';
 import logo from '../../images/crowd-hound-logo-2.png';
 
 class Header extends Component {
+
+  toggleBurger = () => {
+    console.log('okkk')
+  }
   render() {
     return (
       <header className="header">
-        <section className="burger-container">
+        <section className="burger-container" >
+            <button className="burger-btn" onClick={this.toggleBurger}></button>
             <div className="burger-line burger-line-1"></div>
             <div className="burger-line burger-line-2"></div>
             <div className="burger-line burger-line-3"></div>
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
 import './Home.css';
+import dogsHomePage from '../../images/dogs-homepage.jpg';
 
 class Header extends Component {
   render() {
     return (
-      <section className="home">
-        Home
-      </section>
+        <section className="home">
+          <div className="home-part-1">
+            <div className="home-text-container">
+              <h2 className="home-text-title">FIND YOUR PACK</h2>
+              <p className="home-text-body">
+                Lorem ipsum dolor sit amet, 
+                consectetur adipiscing elit,
+                sed do eiusmod tempor
+                incididunt ut labore et dolore
+                magna aliqua.
+              </p>
+            </div>
+            <img src={dogsHomePage} className="homepage-dogs-img" alt="three dogs sitting together"/>
+          </div>
+           <section className="home-carsouel">
+
+          </section>
+        </section>
+        
     )
   }
 }
