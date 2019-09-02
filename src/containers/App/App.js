716ff2097c014a55000ccf08baf2c5c93e5f92f7
“Hindreen” <hndfaw@gmail.com>
2019-09-02 13:20:34 -0600

    Remove extra styles in App

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="app">
-
-    </div>
-  );
+class App extends Component {
+  render() {
+    
+    return (
+      <main className="app">
+  
+      </main>
+    );
+  }
 }
 
 export default App;
