55c241a4bde4bb6665a68af6692dac45c0baa291
“Hindreen” <hndfaw@gmail.com>
2019-09-03 15:22:06 -0600

    Add 404 page to router

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import './App.css';
 import Header from '../Header/Header';
 import Home from '../Home/Home';
+import UserProfile from '../UserProfile/UserProfile';
+import DogProfile from '../DogProfile/DogProfile';
 import { Route, Switch, NavLink } from 'react-router-dom';
 
 
@@ -9,12 +11,22 @@ export class App extends Component {
   render() {
     
     return (
-      <>
-      <Header />
       <main className="app">
-        <Home />
+      <Header />
+        <Switch>
+        <Route exact path="/" render={() => (<Home />)} />
+        <Route exact path="/userprofile" render={() => (<UserProfile />)} />
+        <Route exact path="/dogprofile" render={() => (<DogProfile />)} />
+        <Route render={() => (
+                <>
+                <div className="page-not-found-container">
+                  <h6 className="not-found-status-code">404</h6>
+                  <p className="page-not-exist">The page you’re looking for can’t be found.</p>
+                </div>
+                </>
+            )}/>
+        </Switch>
       </main>
-      </>
     );
   }
 }
