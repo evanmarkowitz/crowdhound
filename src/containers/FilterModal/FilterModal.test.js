68e75fe81b243818326fd6d12d2caa2e529bd251
“Hindreen” <hndfaw@gmail.com>
2019-09-05 19:23:08 -0600

    Filter modal file 100% tested

--- src/containers/FilterModal/FilterModal.test.js
+++ src/containers/FilterModal/FilterModal.test.js
@@ -1,14 +1,72 @@
 import { shallow } from 'enzyme';
-import {FilterModal} from './FilterModal'
-import React from 'react'
+import { FilterModal, mapDispatchToProps } from './FilterModal'
+import React from 'react';
+import { toggleFilterModal } from '../../actions';
 
 describe('FilterModal', () => {
-  let wrapper
+  let wrapper;
+  let props;
+
   beforeEach(() => {
-    wrapper = shallow(<FilterModal />)
+    props = {
+      toggleFilterModal: jest.fn(),
+    }
+    wrapper = shallow(<FilterModal {...props}/>)
   })
   
   it('should match the snapshot', () =>
     expect(wrapper).toMatchSnapshot()
   )
+
+  it('moveSlider should update state', () => {
+    const event = {target: {value: 50}}
+    wrapper.instance().moveSlider(event)
+    expect(wrapper.state().distanceValue).toEqual(50)
+  })
+
+  it('clickFinder should run toggleFilterModal', () => {
+    wrapper.instance().clickFinder();
+    expect(props.toggleFilterModal).toHaveBeenCalled();
+  })
+
+  it('clickFilter should update state', () => {
+    const event = {target: {name: 'size', value: 'small'}}
+    wrapper.instance().clickFilter(event);
+    expect(wrapper.state().size).toEqual('small')
+  })
+
+  it('toggleFilterModal should be called by clicking on element with background className', () => {
+    wrapper.find('.background').simulate('click');
+    expect(props.toggleFilterModal).toHaveBeenCalled();
+  })
+
+  it('should dispatch with a toggleFilterModal action when toggleFilterModal is called', () => {
+    const mockDispatch = jest.fn();
+    const mockSelected = true;
+    const mockAction = toggleFilterModal(mockSelected);
+    const mappedProps = mapDispatchToProps(mockDispatch);
+    mappedProps.toggleFilterModal(mockSelected);
+    expect(mockDispatch).toHaveBeenCalledWith(mockAction);
+  })
+
+  it('activeLevelBtnStyle should return the correct style', () => {
+
+    const event = {target: {name: 'activityLevel', value: 1}}
+    wrapper.instance().clickFilter(event);
+    expect(wrapper.state().activityLevel).toEqual(1)
+
+    const spy = jest.spyOn(wrapper.instance(), "activeLevelBtnStyle");
+    wrapper.instance().forceUpdate();
+    spy(1)
+    expect(spy).toHaveBeenCalledWith(1)
+
+
+  //   const spy = jest.spyOn(wrapper.instance(), "getPalettes");
+  // wrapper.instance().forceUpdate();
+
+  // wrapper.instance().addAndUpdatePalette = jest.fn();
+  // wrapper.instance().addAndUpdatePalette();
+  // expect(spy).toHaveBeenCalled();
+  })
+
 })
