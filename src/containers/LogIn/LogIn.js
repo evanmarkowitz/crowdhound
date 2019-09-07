ba9f31213a5789907e88ad70f80514ad1f8d7664
“Hindreen” <hndfaw@gmail.com>
2019-09-07 15:40:21 -0600

    Add fetching for user log in

--- src/containers/LogIn/LogIn.js
+++ src/containers/LogIn/LogIn.js
@@ -10,7 +10,67 @@ firebase.initializeApp({
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
 })
 
-const LogIn = () => {
+export const AUTHENTICATE_USER = gql`
+
+    mutation authenticateUser($apiKey: String!, $auth: AuthorizationInputType!) {
+      authenticate(apiKey: $apiKey, auth: $auth) {
+        currentUser {
+          id
+          firstName
+          lastName
+          email
+        }
+        new
+      }
+
+    }
+
+
+
+
+    //  {
+    //   authenticateUser(
+    //   apiKey: ${process.env.REACT_APP_USER_API_KEY},
+      // auth: {
+      //   firstName: "Bob",
+      //   lastName: "Smith",
+      //   email: "email@example.com",
+      //   token: "token"
+      // }
+  //   ) {
+  //     currentUser {
+  //       id
+  //       firstName
+  //       lastName
+  //       email
+  //     }
+  //     new
+  //   }
+  // }
+  `;
+
+
+
+export const LogIn = () => {
+
+  const auth = {
+    firstName: "Bob",
+    lastName: "Smith",
+    email: "email@example.com",
+    token: "token"
+  }
+
+  const { loading, error, data } = useQuery(
+    AUTHENTICATE_USER,
+    {variables: { apiKey: process.env.REACT_APP_USER_API_KEY, auth }}
+  )
+
+  console.log(error, data)
+
+  if(loading) return <p>Loading....</p>;
+  if(error) return <p>Error :</p>;
+
+
 
    const uiConfig = {
     signInFlow: 'popup',
