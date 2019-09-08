033b92092ec7c94a3a21e243f579de332f0e145c
Evan Markowitz <emarkowitz1@gmail.com>
2019-09-08 09:09:32 -0600

    Add reponsiveness to carousel

--- src/containers/DogCarousel/DogCarousel.js
+++ src/containers/DogCarousel/DogCarousel.js
@@ -49,10 +49,36 @@ export const DogCarousel = () => {
     speed: 500,
     centerMode: true,
     slidesToShow: 3,
-    adaptiveHeight: false,
+    adaptiveHeight: true,
     arrows: true,
     prevArrow: <SamplePrevArrow />,
-    nextArrow: <SampleNextArrow />
+    nextArrow: <SampleNextArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
     };
 
   const { loading, error, data } = useQuery(
