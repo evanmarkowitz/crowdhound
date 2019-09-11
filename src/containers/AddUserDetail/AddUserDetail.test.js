aff95c13534d65f1ed0e690df6efc256cdb05e4b
“Hindreen” <hndfaw@gmail.com>
2019-09-10 21:34:45 -0600

    Add more tests for app and addUserDetail

--- src/containers/AddUserDetail/AddUserDetail.test.js
+++ src/containers/AddUserDetail/AddUserDetail.test.js
@@ -15,5 +15,17 @@ describe('AddUserDetail', () => {
     expect(wrapper).toMatchSnapshot()
   )
 
+  it('handleChange should set the state', () => {
+    const e = {target: {name: 'description', value: 'This is my description'}};
+    wrapper.instance().handleChange(e);
+    expect(wrapper.state().description).toEqual('This is my description')
+  })
+
+  it('handleFiles should call sendPhoto', () => {
+    wrapper.instance().sendPhoto = jest.fn();
+    wrapper.instance().handleFiles({files: {base64: 'base'}});
+    expect(wrapper.instance().sendPhoto).toHaveBeenCalled();
+  })
+
 
 })
